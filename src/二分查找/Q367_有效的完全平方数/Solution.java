4fc837c281b2f5eba8ac73180a91ac9cd239b697
himcs <787632628@qq.com>
2021-01-12 21:55:05 +0800

    - 20210112 [367. 有效的完全平方数](https://leetcode-cn.com/problems/valid-perfect-square/)

---
+++ src/二分查找/Q367_有效的完全平方数/Solution.java
@@ -0,0 +1,20 @@
+package 二分查找.Q367_有效的完全平方数;
+
+public class Solution {
+  public boolean isPerfectSquare(int num) {
+    int low = 1;
+    int high = num;
+    while (low <= high) {
+      int mid = low + ((high - low) >>> 1);
+      long val = ((long) mid) * mid;
+      if (val == num) {
+        return true;
+      } else if (val > num) {
+        high = mid - 1;
+      } else {
+        low = mid + 1;
+      }
+    }
+    return false;
+  }
+}
