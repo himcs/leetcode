88a3a8d3c551a79e631590a45de7317866e2ccc4
himcs <mcshimcs@gmail.com>
2022-03-21 11:12:10 +0800

    Q4

--- src/链表/Q21_合并两个有序链表/Solution.java
+++ src/链表/Q21_合并两个有序链表/Solution.java
@@ -1,44 +1,45 @@
 package 链表.Q21_合并两个有序链表;
 
 public class Solution {
-  public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
-    if (l1 == null) {
-      return l2;
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        ListNode p = list1;
+        ListNode q = list2;
+        ListNode z = new ListNode(-1);
+        z.next =  new ListNode(-1);
+        ListNode zH = z;
+        while (p != null && q != null) {
+            if (p == null) {
+                z.next = q;
+                q = q.next;
+            } else if (q == null) {
+                z.next = p;
+                p = p.next;
+            } else {
+                if (q.val <= p.val) {
+                    z.next = q;
+                    q = q.next;
+                } else {
+                    z.next = p;
+                    p = p.next;
+                }
+            }
+            z=z.next;
+        }
+        return zH.next;
     }
-    if (l2 == null) {
-      return l1;
-    }
-    ListNode head;
-    ListNode start;
-    if (l1.val > l2.val) {
-      start = head = l2;
-      l2 = l2.next;
-    } else {
-      start = head = l1;
-      l1 = l1.next;
-    }
-    while (l1 != null && l2 != null) {
-      if (l1.val < l2.val) {
-        head.next = l1;
-        head = l1;
-        l1 = l1.next;
-      } else {
-        head.next = l2;
-        head = l2;
-        l2 = l2.next;
-      }
-    }
-    if (l1 != null) {
-      head.next = l1;
-    } else if (l2 != null) {
-      head.next = l2;
-    }
-    return start;
-  }
 
-  public static void main(String[] args) {
-    ListNode l1 = new ListNode(2);
-    ListNode l2 = new ListNode(1);
-    new Solution().mergeTwoLists(l1, l2);
-  }
+    public static void main(String[] args) {
+        ListNode la1 = new ListNode(1);
+        ListNode la2 = new ListNode(2);
+        ListNode la3 = new ListNode(3);
+        la1.next=la2;
+        la2.next = la3;
+        ListNode lb1 = new ListNode(1);
+        ListNode lb2 = new ListNode(3);
+        ListNode lb3 = new ListNode(4);
+        lb1.next=lb2;
+        lb2.next = lb3;
+
+        new Solution().mergeTwoLists(la1, la1);
+    }
 }
