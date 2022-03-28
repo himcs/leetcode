7c6de850b9ae35b8e492d1cf56fb3463ea42d662
himcs <mcshimcs@gmail.com>
2022-03-28 10:58:47 +0800

    Q31

---
+++ src/双指针/Q31_下一个排列/Solution.java
@@ -0,0 +1,37 @@
+package 双指针.Q31_下一个排列;
+
+import java.util.Arrays;
+
+public class Solution {
+    public void nextPermutation(int[] nums) {
+        int len = nums.length;
+        for (int i = len - 1; i >= 0; i--) {
+            for (int j = len - 1; j > i; j--) {
+                if (nums[j] > nums[i]) {
+                    swap(nums, i, j);
+                    Arrays.sort(nums, i + 1, len);
+                    return;
+                }
+            }
+        }
+        Arrays.sort(nums);
+        return;
+    }
+
+    private void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+//123
+//    132
+
+    public static void main(String[] args) {
+//        [4,2,0,2,3,2,0]
+
+//        [4,2,0,3,0,2,2]
+
+//        [4,2,2,0,0,2,3]
+        new Solution().nextPermutation(new int[]{1, 2, 3});
+    }
+}
