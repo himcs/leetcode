80925606887605994afb622b9e07e2b1a96ab923
himcs <mcshimcs@gmail.com>
2022-05-29 16:52:14 +0800

    Q101_对称二叉树

---
+++ src/二叉树/Q101_对称二叉树/Solution.java
@@ -0,0 +1,23 @@
+package 二叉树.Q101_对称二叉树;
+
+import 二叉树.TreeNode;
+
+public class Solution {
+    public boolean isSymmetric(TreeNode root) {
+        return check(root, root);
+    }
+    public boolean check(TreeNode p, TreeNode q)
+    {
+        if(p == null && q == null)
+        {
+            return true;
+        }
+        if(p == null || q == null)
+        {
+            return false;
+        }
+        return p.val == q.val &&
+                check(p.left, q.right) &&
+                check(p.right, q.left);
+    }
+}
